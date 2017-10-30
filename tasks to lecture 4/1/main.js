5451899dd2d10cc01a34879c1a80db01b597bc6d
Kosianchuk <kosianchuk.d@gmail.com>
2017-10-30 22:54:46 +0200

    changes in task 1 to lecture 4

--- tasks to lecture 4/1/main.js
+++ tasks to lecture 4/1/main.js
@@ -1,11 +1,10 @@
 var A = [23,1,2,52,5,34,23,6,246],
     hub;
 
-for(var i = 0; i < A.length-1; i++){
+for(var i = 0; i < A.length-1; i+2){
   hub = A[i];
   A[i] = A[i + 1];
   A[i + 1] = hub;
-  i++;
 }
 
 console.log(A);
