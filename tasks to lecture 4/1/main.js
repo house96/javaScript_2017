9bacc30c65cbc530b64cbea354d70ffbcf0e23b0
Kosianchuk <kosianchuk.d@gmail.com>
2017-10-31 09:44:52 +0200

    add task 1 to lecture 5

--- tasks to lecture 4/1/main.js
+++ tasks to lecture 4/1/main.js
@@ -1,7 +1,7 @@
 var A = [23,1,2,52,5,34,23,6,246],
     hub;
 
-for(var i = 0; i < A.length-1; i+2){
+for(var i = 0; i < A.length-1; i+=2){
   hub = A[i];
   A[i] = A[i + 1];
   A[i + 1] = hub;
---
+++ tasks to lecture 5/1/main.js
@@ -0,0 +1,17 @@
+
+function fillingArray(arr, m) {
+  for(var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(m);
+    for(var j = 0; j < arr[i].length; j++) {
+      arr[i][j] = Math.floor(Math.random()*50);
+    }
+  }
+  
+console.log(arr);
+}
+
+function creatArray(n, m) {
+  var arr = new Array(n);
+    fillingArray(arr, m);
+}
+creatArray(9, 6);
