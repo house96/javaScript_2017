7a08de849579f98ac922e284049f0ff36dac28da
Kosianchuk <kosianchuk.d@gmail.com>
2017-11-06 12:00:58 +0200

    change  task to 6 lecture

--- tasks to lecture 6/1/main.js
+++ tasks to lecture 6/1/main.js
@@ -1,5 +1,4 @@
 function fofoFunction() {
-<<<<<<< HEAD
   var result = new Array();
 
   for (var i = 0; i < arguments.length; i++) {
@@ -17,22 +16,3 @@ function fofoFunction() {
 }
 
 console.log(fofoFunction([1, 2, 3], [3, 6, 3223], [2, 8, 5, 7, 457, 457, 4], [22, 77, 99, 44]));
-=======
-  result = new Array();
-
-  for (var i = 0; i <  arguments.length; i++) {
-    var centr = Math.ceil(arguments[i].length / 2 - 1);
-
-    if(arguments[i].length % 2 !== 0) {
-       result.push(arguments[i][centr]);
-    } if(arguments[i].length % 2 == 0) {
-      result = result.concat(arguments[i].splice(centr, 2));
-    }
-  
-}
-
-}
-
-fofoFunction([1, 2, 3], [3, 6, 3223], [2, 8, 5, 7, 457, 457, 4], [22, 77, 99, 44]);
-console.log(result);
->>>>>>> 9878ed490dc0a78a249fb5f736919df6695f24b1
