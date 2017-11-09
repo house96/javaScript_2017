45a43d7e4aecf24835223f5f3e69e13032a3b2dc
Kosianchuk <kosianchuk.d@gmail.com>
2017-11-09 18:05:20 +0200

    cheng the 1 task to 7 lecture

--- tasks to lecture 7/1/main.js
+++ tasks to lecture 7/1/main.js
@@ -2,14 +2,24 @@ function sequence(x, y) {
   var start = x || 0,
     step = y || 1;
 
-  return increment(start, step);
+  if (step == 1) {
+    increment(start, step);
+
+  } else {
+    for (var i = 0; i < step; i++) {
+
+      start = increment(start, step);
+    }
+  }
+
 }
 
 function increment(start, step) {
-  console.log(start += step);
-  return sequence(start, step);
-}
 
+  console.log(start += step);
+  return start += step;
+
+}
 
 //
 sequence(2, 3);
