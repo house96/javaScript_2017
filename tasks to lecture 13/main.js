be3d6c1e421d5a2af7cc02f8467305afdfd05605
Dimitriy Kosianchuk <kosianchuk.d@gmail.com>
2017-12-02 23:35:17 +0200

    add task to 13 lecture

--- tasks to lecture 13/main.js
+++ tasks to lecture 13/main.js
@@ -1,21 +1,20 @@
 (function () {
+  var block = document.querySelector('.block');
   var btn = document.querySelector('.btn');
   var input = document.querySelectorAll('.input');
   var redCheckbox = document.querySelectorAll('.input-red');
   var blueCheckbox = document.querySelectorAll('.input-blue');
   var isBlue = true;
 
-  for (var i = 0; i < input.length; i++) {
-    input[i].addEventListener('change', function inputStatus(e) {
-      if (this.classList.contains('input-red')) {
-        statRed();
-      }
-      if (this.classList.contains('input-blue')) {
-        statBlue();
-      }
-    })
-  };
 
+  window.addEventListener("load", function () {
+    for (var i = 0; i < input.length; i++) {
+      input[i].addEventListener('change', function inputStatus(e) {
+        this.classList.contains('input-red') ? statRed() : statBlue();
+      });
+    };
+    block.addEventListener('mousedown', clickBlock);
+  });
 
   function statBlue() {
     for (var i = 0; i < blueCheckbox.length; i++) {
@@ -23,13 +22,11 @@
       if (blueCheckbox[i].checked) {
         removeEventBtn();
         return isBlue = false;
-
-      }
+      };
       isBlue = true;
       statRed();
     }
   };
-
 
   function statRed() {
     var sumRed = 0;
@@ -39,19 +36,13 @@
 
         sumRed += 1;
       }
-    }
+    };
     analisis(sumRed);
   };
 
-
   function analisis(sumRed) {
-    if (sumRed > 1 && isBlue) {
-
-      return btn.addEventListener('click', printSomeText);
-    }
-    removeEventBtn();
+    (sumRed > 1 && isBlue)? btn.addEventListener('click', printSomeText) : removeEventBtn();
   };
-
 
   function printSomeText() {
     console.log("My code works, but I don't know why");
@@ -59,5 +50,24 @@
 
   function removeEventBtn() {
     btn.removeEventListener('click', printSomeText);
+  };
+
+  function clickBlock() {
+    if (event.shiftKey) {
+      drag(event);
+      block.addEventListener('mouseup', onmouseup);
+    }
+  };
+
+  function drag(e) {
+    block.style.left = e.pageX - block.offsetWidth / 2 + 'px';
+    block.style.top = e.pageY - block.offsetHeight / 2 + 'px';
+
+    document.addEventListener('mousemove', drag)
+  };
+
+  function onmouseup() {
+    document.removeEventListener('mousemove', drag);
+
   };
 })();
